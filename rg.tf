4f2079fbdf6d8e5caf962b5818a204e00d5fcff1
Siddu-28design <siddusteli08353@gmail.com>
2025-08-15 01:47:06 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -25,7 +25,7 @@ tenant_id="7e1b4521-93b6-473d-8e77-cf32c35bff05"
 subscription_id="34a1c0d5-ab81-462c-89db-795e1572cd70"
 }
 variable "rgname" {
-  default = "Terraformcloudrg"
+  default = "Terraformcloudrg-Siddu"
 }
 
 variable "location" {
